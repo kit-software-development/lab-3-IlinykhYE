d2baf60cbc331fdaa9d00fc5183faf65ddf780d3
IlinykhYE <47817626+IlinykhYE@users.noreply.github.com>
2019-06-03 23:30:36 +0300

    Add files via upload

---
+++ Button/Main_dashboard.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Button
+{
+    partial class Main_dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button.Location = new System.Drawing.Point(109, 107);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(103, 42);
+            this.button.TabIndex = 0;
+            this.button.Text = "Push me";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.OnClick);
+            // 
+            // Main_dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 256);
+            this.Controls.Add(this.button);
+            this.MinimumSize = new System.Drawing.Size(150, 150);
+            this.Name = "Main_dashboard";
+            this.Text = "Running button";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+    }
+}
+
